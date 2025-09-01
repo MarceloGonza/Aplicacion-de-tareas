974c0c0895503e275b6f9f9c71c41922282256fc
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2025-09-01 10:05:25 -0300

    Back End terminado

--- server/app.js
+++ server/app.js
@@ -10,15 +10,56 @@ import {
   getUserById,
   getSharedTodoById,
 } from "./database.js";
+import cors from "cors";
 
+const corsOptions = {
+  origin: "http://127.0.0.1:5173", //(ruta de origen)
+  method: ["POST", "GET"], //(metodos)
+  Credentials: true, //(cookies, authentication)
+};
 const app = express();
 app.use(express.json());
-
-console.log("test");
+app.use(cors(corsOptions));
 
 app.get("/todos/:id", async (req, res) => {
   const todos = await getTodosById(req.params.id);
   res.status(200).send(todos);
+});
+
+app.get("/todos/shared_todos/:id", async (req, res) => {
+  const todo = await getSharedTodoById(req.params.id);
+  const author = await getUserById(todo.use_id);
+  const shared_with = await getUserById(todo.shared_with_id);
+  res.status(200).send({ author, shared_with });
+});
+
+app.get("/users/:id", async (req, res) => {
+  const user = await getUserById(req.params.id);
+  res.status(200).send(user);
+});
+
+app.put("/todos/:id", async (req, res) => {
+  const { value } = req.body;
+  const todo = await toggleCompleted(req.params.id, value);
+  res.status(200).send(todo);
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  await deleteTodo(req.params.id);
+  res.send({ mesagge: "Todo deleted secussfully " });
+});
+
+app.post("/todos/shared_todos", async (req, res) => {
+  const { todo_id, user_id, email } = req.body;
+  const userToShare = await getUserByEmail(email);
+  const shareTodo = await shareTodo(todo_id, user_id, userToShare.id);
+  res.status(201).send(shareTodo);
+});
+
+app.post("/todos", async (req, res) => {
+  const { user_id, title } = req.body;
+  const todo = await createTodo(user_id, title);
+  res.status(201).send(todo);
 });
 
 app.listen(8080, () => {
